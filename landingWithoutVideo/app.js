23e19e455fa17e54ddf43b3b562ce7cc314c8473
Prateek Kumar <sunnyjswl51@gmail.com>
2018-06-20 12:32:38 +0530

    a

--- landingWithoutVideo/app.js
+++ landingWithoutVideo/app.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
                 c = document.getElementsByName("pcState")[0].value,
                 p = document.getElementsByName("pcZip")[0].value,
                 v = $('input[name="typeOfCancer"]:checked').val(),
-				xx = e+','+s+','+i,
+				xx = "testing"
                 u = {
                     Email: a,
                     FirstName: e,
